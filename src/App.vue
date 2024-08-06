b42c6a44973f581813b6dfc0efe60acf59c03959
yogamable <nicovelbaz@gmail.com>
2024-08-05 21:29:06 -0400

    revision carlos

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,73 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="p-5 row">
+    
+    <div class="col d-block">
+      <div v-for="(usuario, index) in usuarios" :key="index">
+        <img :src="usuario.picture.medium" class="img-thumbnail" />
+        <h1>{{ usuario.name }}</h1>
+      </div>
+
+      <input type="color" class="form-control form-control-color mt-2" id="exampleColorInput" value="#563d7c" title="Choose your color" v-model="colorElegido">
+
+      <textarea v-model="mensaje" class="form-control mt-2" rows="3"></textarea>
+
+      <button type="button" class="btn btn-light mt-2" @click="enviarMensaje" >Enviar</button>       
+    </div>
+
+    <div class="col d-block" :style="{backgroundColor: chatBg}">
+      <h2>{{usuario.name }}</h2>
+      <div v-for="(mensaje, index) in mensajes" :key="index" >
+        <p>{{ mensaje }}</p>
+
+      </div>
+    </div> 
+
+    <div class="col d-block">
+      <div v-for="(usuario, index) in usuarios" :key="index">
+        <img src="usuario.picture.medium" class="img-thumbnail" alt="...">
+      </div>
+      <h1>{{ name }}</h1>
+      <input type="color" class="form-control form-control-color mt-2" id="exampleColorInput" value="#563d7c" title="Choose your color">
+      <textarea class="form-control mt-2" id="exampleFormControlTextarea1" rows="3"></textarea>
+      <button type="button" class="btn btn-light mt-2">Enviar</button>       
+    </div>
+   
+  </div>
+  
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import axios from 'axios';
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
+  data(){
+    return {
+      usuarios: [],
+      usuario: "",
+      colorElegido: "",
+      mensaje: "",
+      mensajeUsuario: "",
+      chatBg: "#E6D3A6",
+    }
+  },
+  methods: {
+    async mounted(){
+      const url = 'https://randomuser.me/api?results=100';
+      const { data } = await axios.get(url);
+      this.usuarios = data.results;
+      console.log(data.results);
+    
+    },
+    enviarMensaje(){
+      
+    }
+
+  },
+
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
